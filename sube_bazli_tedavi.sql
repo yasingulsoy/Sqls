f8252f927b58c4d90a1c10194e9303a255efcb43
hburaka <hburaka@gmail.com>
2025-08-28 12:51:50 +0300

    Update sube_bazli_tedavi.sql
    field isimleri turkcelestirildi ve tedavi ana kategori ile alt kategori where satırlarına eklendi.

--- sube_bazli_tedavi.sql
+++ sube_bazli_tedavi.sql
@@ -1,45 +1,52 @@
 SELECT
     t.id,
-    t.plan_date,
-    t.procedure_category_name,
-    pgs.subcat_name AS procedure_subcat_name,    
-    t.procedure_name,
-    t.currency,
-    ROUND(t.unit_price * t.rate, 2) AS treatment_amount_tl,
-    ROUND(t.unit_price, 2) AS unit_price,
-    ROUND(t.total_price, 2) AS total_price,
-    ROUND(t.ins_discount_percentage, 2)  AS ins_discount_percentage,
-    ROUND(t.cc_discount_percentage, 2)   AS cc_discount_percentage,
-    ROUND(t.cash_discount_percentage, 2) AS cash_discount_percentage,
-    t.payment_distribution,
-    t.company_payment,
-    date(t.saved_on) as saved_on,
-    date(t.updated_on) as updated_on,
-    c.id as clinicId,
-    c.clinic_name,
-    cur.exchange,
-    TIMESTAMPDIFF(year, p.birthday, CURDATE()) AS patientAge,
-    p.id as patientId,
-    concat(p.first_name, ' ', p.last_name) as patientName,
-    pd.id as planDoctorId,
-    CONCAT(pd.first_name, ' ', pd.last_name) AS planDoctorFullname,
-    u.id AS doctorId, 
-    CONCAT(u.first_name,  ' ', u.last_name )  AS doctorFullname,
-    p.partnership_companies,    
-    pc.pc_name,
-    pc.company_type,
+    t.plan_date as plan_tarihi,
+    pgct.id as tedavi_kategori_id,
+    t.procedure_category_name as tedavi_kategori,
+    pgs.id as tedavi_alt_kategori_id,
+    pgs.subcat_name AS tedavi_alt_kategori,    
+    t.procedure_name as tedavi,
+    t.plan_teeth as dis_no,
+    t.currency as para_birim_id,
+	cur.exchange as para_birimi,
+	t.rate as doviz_kuru,
+    ROUND(t.unit_price * t.rate, 2) AS tedavi_toplam_tl,
+    ROUND(t.unit_price, 2) AS tedavi_birim_fiyati,
+    ROUND(t.total_price, 2) AS tedavi_toplam,
+    ROUND(t.ins_discount_percentage, 2)  AS taksit_indirim_yuzde,
+    ROUND(t.cc_discount_percentage, 2)   AS tekcekim_indirim_yuzde,
+    ROUND(t.cash_discount_percentage, 2) AS nakit_indirim_yuzde,
+    t.payment_distribution as odeme_dagitim,
+    t.company_payment as kurum_odemesi_mi,
+    date(t.saved_on) as kayit_tarihi,
+    date(t.updated_on) as guncelleme_tarihi,
+    t.is_refunded,
+    t.is_rpt, 
+    t.is_examination as muayene_mi,
+    c.id as sube_id,
+    c.clinic_name as sube_adi,
+    TIMESTAMPDIFF(year, p.birthday, CURDATE()) AS hasta_yasi,
+    p.id as hasta_id,
+    concat(p.first_name, ' ', p.last_name)  as hasta_adi,
+    pd.id as plani_yapan_hekim_id,
+    CONCAT(pd.first_name, ' ', pd.last_name) AS plani_yapan_hekim,
+    u.id AS tedavi_yapan_hekim_id, 
+    CONCAT(u.first_name,  ' ', u.last_name )  AS tedavi_yapan_hekim,
+    p.partnership_companies as ak_id,    
+    pc.pc_name as ak_adi,
+    pc.company_type as ak_tipi_id,
       CASE
         WHEN pc.company_type = 1 THEN 'Yurtiçi Kurum'
         WHEN pc.company_type = 2 THEN 'Yurtdışı Kurum'
         WHEN pc.company_type IS NULL THEN 'Kurum Yok'
         ELSE NULL
-      END AS company_type_text,    
-    nt.id as patient_nationality_typeId,
-    nt.type_name AS patient_nationality_type,
-    dbc.id as doctorBranchId,
-    dbc.branch_name AS doctorBranchname,
-    h.id,
-    h.hdyhau_name
+      END AS ak_tipi_adi,    
+    nt.id as vatandaslik_tipi_id,
+    nt.type_name AS vatandaslik_tipi,
+    dbc.id as hekim_brans_id,
+    dbc.branch_name AS hekim_brans,
+    h.id as gelis_sekli_id,
+    h.hdyhau_name as gelis_sekli_adi
 FROM treatments t
 JOIN patients p ON p.id = t.patient_id
 JOIN users u ON u.id = t.doctor_id
@@ -54,7 +61,7 @@ LEFT JOIN nationality_types nt ON nt.id = p.nationality_type
 LEFT JOIN doctor_branch_codes dbc ON dbc.id = u.doctor_branch_code
 LEFT JOIN hdyhau h ON h.id = p.hdyhau
 WHERE
-    t.treatment_type = 3
+	t.treatment_type = 3
     AND t.is_deleted = 0
     AND t.is_done = 1
     AND (:has_clinic_id = 0 OR t.clinic_id IN (:clinic_id))
@@ -66,5 +73,7 @@ WHERE
     AND (:has_doctor_branch_code     = 0 OR dbc.id IN (:doctor_branch_code))
     AND (:has_hdyhau_id              = 0 OR h.id IN (:hdyhau_id))
 	AND (:has_company_type = 0 
-  	OR pc.company_type IN (:company_type) 
-  	OR (pc.company_type IS NULL AND 'null' IN (:company_type)))
+	  	OR pc.company_type IN (:company_type) 
+	  	OR (pc.company_type IS NULL AND 'null' IN (:company_type)))
+  	AND (:has_price_group_category_treatments = 0 OR pgct.id IN (:has_price_group_category_treatments))
+  	AND (:has_price_group_subcategories = 0 OR pgs.id IN (:has_price_group_subcategories))
